10e69c60845a44e9b174d4e8000118454af92255
Dines Pc Portable <ndinespro@gmail.com>
2023-10-24 13:24:04 +0200

    changement de question 3 efféctué + ne pas tenir compte de la ligne 25 je m'en oqp dans une heure

--- Automates_Complet.py
+++ Automates_Complet.py
@@ -5,31 +5,24 @@ alphabet = ['a', 'b']
 q0 = State('q0', True, False, {'a': 'q3', 'b': 'q1'})
 q1 = State('q1', False, False, {'a': 'q1', 'b': 'q2'})
 q2 = State('q0', True, False, {'a': 'q7', 'b': 'q4'})
-q3 = State('q1', False, False, {'a': 'q7'})
+q3 = State('q1', False, False, {'a': 'q7' , 'b': 'a7'})
 
 
 states = [q0, q1, q2, q3]
-
-
+          
 def is_complete(self):
         is_complete=True
         for state in states:
             if state.isFinal:
                 if set(state.transitions.keys()) != set(alphabet):
                     is_complete = False
-                    print(f"L'état {state.name} n'est pas complet.")
-                else:
-                    print(f"L'état {state.name} est complet.")
             else:
                 if set(state.transitions.keys()) != set(alphabet):
                     is_complete = False
-                    print(f"L'état {state.name} n'est pas complet.")
-                else:
-                    print(f"L'état {state.name} est complet.")
-        if is_complete:
+        if is_complete == True:
             print("L'automate est complet.")
         else:
-            print("L'automate n'est pas complet.")
+            #fonction à rajouter
+            print("L'automate n'était pas complet, il l'est désormais.")
         return is_complete 
 is_complete(Automate)
-
