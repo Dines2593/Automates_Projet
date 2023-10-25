33e01a686210c9829b3f7a6d691933fda84c9f35
Dines Pc Portable <ndinespro@gmail.com>
2023-10-25 12:00:08 +0200

    Réalisation de la fonction make complete (q4) plus correction de la q3

--- Automates_Complet.py
+++ Automates_Complet.py
@@ -4,13 +4,14 @@ from classes import State
 alphabet = ['a', 'b']
 q0 = State('q0', True, False, {'a': 'q3', 'b': 'q1'})
 q1 = State('q1', False, False, {'a': 'q1', 'b': 'q2'})
-q2 = State('q0', True, False, {'a': 'q7', 'b': 'q4'})
-q3 = State('q1', False, False, {'a': 'q7' , 'b': 'a7'})
+q2 = State('q2', True, False, {'a': 'q7', 'b': 'q4'})
+q3 = State('q3', False, False, {'a': 'q7', 'b' : 'phi'})
 
 
 states = [q0, q1, q2, q3]
-          
-def is_complete(self):
+
+
+def is_complete(Automate):
         is_complete=True
         for state in states:
             if state.isFinal:
@@ -22,7 +23,9 @@ def is_complete(self):
         if is_complete == True:
             print("L'automate est complet.")
         else:
-            #fonction à rajouter
-            print("L'automate n'était pas complet, il l'est désormais.")
+            print("L'automate n'est pas complet.")
         return is_complete 
 is_complete(Automate)
+
+
+
---
+++ test.py
@@ -0,0 +1,70 @@
+from classes import Automate
+from classes import State
+
+alphabet = ["a", "b"]
+q0 = State("q0", True, False, {"a": "q3", "b": "q1"})
+q1 = State("q1", False, False, {"a": "q1", "b": "q2"})
+q2 = State("q2", False, False, {"a": "q7", "b": "q4"})
+q3 = State("q3", False, False, {"a": "q7"})
+
+states = [q0, q1, q2, q3]
+
+automate = Automate(alphabet, states)
+
+
+def make_complete(automate):
+    if is_complete(automate) == True:
+         print("L'automate est déja complet ou a été complété.")
+         return
+    else:
+        phi_state = State("phi", False, False, {})
+        automate.states.append(phi_state)
+        #print_states(automate)
+        for state in automate.states:
+            for symbol in alphabet:
+                if symbol not in state.transitions:
+                    state.transitions[symbol] = "phi"
+        #print_automate.states(automate)
+        for state in automate.states:
+            for symbol, target_state in state.transitions.items():
+                if target_state == "phi":
+                    state.transitions[symbol] = phi_state.name
+
+def is_complete(automate):
+        is_complet=True
+        for state in automate.states:
+            if state.isFinal:
+                if set(state.transitions.keys()) != set(alphabet):
+                    is_complet = False
+            else:
+                if set(state.transitions.keys()) != set(alphabet):
+                    is_complet = False
+        if is_complet == True:
+            print("L'automate est complet.")
+        else:
+            print("L'automate n'est pas complet.")
+        return is_complet
+
+
+"""
+def print_states(automate):
+    for state in states:
+        print(f"État : {state.name}")
+        print(f"Transitions : {state.transitions}")
+        print(f"Est initial : {state.isInitial}")
+        print(f"Est final : {state.isFinal}")
+
+
+print("États de l'automate avant la complétion :")
+print_states(automate)
+print("\n")
+is_complete(automate)
+print("États de l'automate après la complétion :")
+print("\n")
+print_states(automate)
+is_complete(automate)
+
+"""
+
+make_complete(automate)
+make_complete(automate)
