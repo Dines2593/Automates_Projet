cdff76098f8139045488fd0bd3d633108ca15e6a
sara-molie <125445622+sara-molie@users.noreply.github.com>
2023-12-04 18:29:53 +0100

    modif enlever les classe fini

--- miroir.py
+++ miroir.py
@@ -1,34 +1,29 @@
-from classes import State
+from classes import State, Automate
+from affichage import draw_automaton
 
-class Automate:
-    def __init__(self, alphabet, states, name):
-        self.alphabet = alphabet
-        self.states = states
-        self.name = name
+def mirror(states, alphabet, name):
+    mirrored_states = []
 
-    def mirror(self):
-        mirrored_states = []
+    # Inverser les états finaux et non finaux
+    for state in states:
+        mirrored_state = State(state.name, state.isFinal, state.isInitial, {})
+        mirrored_states.append(mirrored_state)
 
-        # Inverser les états finaux et non finaux
-        #Parcourt tous les états de l'automate d'origine et crée un nouvel état miroir et d'inverser les valeurs final et initial pour chaque état miroir.
-        for state in self.states:
-            mirrored_state = State(state.name, state.isFinal, state.isInitial, {})
-            mirrored_states.append(mirrored_state)
+    # Mettre à jour les transitions des états miroirs
+    #pour chaque etat de l'etat miroir, et pour chaque symbole de l'alphabet alors on le met dans une transition 
+    for state in mirrored_states:
+        for symbol in alphabet:
+            new_transitions = []
+            for target_state in states:
+                if state.name in target_state.transitions.get(symbol, []):
+                    new_transitions.append(target_state.name)
+            state.transitions[symbol] = new_transitions
 
-        #Parcourt tout les états miroirs créés précédemment et les transitions de l'automate d'origine. 
-        #Pour chaque symbole de l'alphabet, il met à jour les transitions de l'état miroir en fonction des transitions de l'automate d'origine.
-        for state in mirrored_states:
-            for symbol in self.alphabet:
-                new_transitions = []
-                for target_state in self.states:
-                    if state.name in target_state.transitions.get(symbol, []):
-                        new_transitions.append(target_state.name)
-                state.transitions[symbol] = new_transitions
+    # Créer et retourner l'automate miroir
+    mirrored_automaton = Automate(alphabet, mirrored_states, name + '_mirror')
+    return mirrored_automaton
 
-        # Créer et retourner l'automate miroir
-        mirrored_automaton = Automate(self.alphabet, mirrored_states, self.name + '_mirror')
-        return mirrored_automaton
-    
+#on affiche le new automate 
 def print_states(result_automate):
     for state in result_automate.states:
         transitions_str = ', '.join([f"{symbol}: {transitions}" for symbol, transitions in state.transitions.items() if transitions])
@@ -36,8 +31,8 @@ def print_states(result_automate):
             print(f"État : {state.name}")
             if transitions_str:
                 print(f"Transitions : {transitions_str}")
-            print(f"Est initial : {state.isInitial}")
-            print(f"Est final : {state.isFinal}")
+            print(f"Initial : {state.isInitial}")
+            print(f"Final : {state.isFinal}")
             print()
 
 # Exemple d'utilisation
@@ -49,6 +44,7 @@ q2 = State('q2', False, True, {})
 q3 = State('q3', False, True, {'a': ['q3']})
 
 automaton = Automate(alphabet, [q0, q1, q2, q3], 'autobahn')
-mirrored_automaton = automaton.mirror()
+mirrored_automaton = mirror(automaton.states, automaton.alphabet, automaton.name)
 
 print_states(mirrored_automaton)
+draw_automaton(mirrored_automaton)
