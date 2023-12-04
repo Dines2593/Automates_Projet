b2455cbe9835fe75956f9f3d8862b3bd551c8e86
sara-molie <125445622+sara-molie@users.noreply.github.com>
2023-12-04 14:23:07 +0100

    changement print

--- miroir.py
+++ miroir.py
@@ -1,9 +1,4 @@
-class State:
-    def __init__(self, name, initial, final, transitions):
-        self.name = name
-        self.initial = initial
-        self.final = final
-        self.transitions = transitions
+from classes import State
 
 class Automate:
     def __init__(self, alphabet, states, name):
@@ -17,10 +12,10 @@ class Automate:
         # Inverser les états finaux et non finaux
         #Parcourt tous les états de l'automate d'origine et crée un nouvel état miroir et d'inverser les valeurs final et initial pour chaque état miroir.
         for state in self.states:
-            mirrored_state = State(state.name, state.final, state.initial, {})
+            mirrored_state = State(state.name, state.isFinal, state.isInitial, {})
             mirrored_states.append(mirrored_state)
 
-        #Parcourt tous les états miroirs créés et les transitions de l'automate d'origine. 
+        #Parcourt tout les états miroirs créés précédemment et les transitions de l'automate d'origine. 
         #Pour chaque symbole de l'alphabet, il met à jour les transitions de l'état miroir en fonction des transitions de l'automate d'origine.
         for state in mirrored_states:
             for symbol in self.alphabet:
@@ -33,6 +28,17 @@ class Automate:
         # Créer et retourner l'automate miroir
         mirrored_automaton = Automate(self.alphabet, mirrored_states, self.name + '_mirror')
         return mirrored_automaton
+    
+def print_states(result_automate):
+    for state in result_automate.states:
+        transitions_str = ', '.join([f"{symbol}: {transitions}" for symbol, transitions in state.transitions.items() if transitions])
+        if transitions_str or state.isInitial or state.isFinal:
+            print(f"État : {state.name}")
+            if transitions_str:
+                print(f"Transitions : {transitions_str}")
+            print(f"Est initial : {state.isInitial}")
+            print(f"Est final : {state.isFinal}")
+            print()
 
 # Exemple d'utilisation
 alphabet = ['a', 'b']
@@ -45,7 +51,4 @@ q3 = State('q3', False, True, {'a': ['q3']})
 automaton = Automate(alphabet, [q0, q1, q2, q3], 'autobahn')
 mirrored_automaton = automaton.mirror()
 
-# Afficher l'automate miroir
-for state in mirrored_automaton.states:
-    print(f"State: {state.name}, Initial: {state.initial}")
-    print(f"Final: {state.final}, Transitions: {state.transitions}")
+print_states(mirrored_automaton)
