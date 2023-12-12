4974ef0f3fef1101cb48e52652257fe00864ddd9
SCL3 <106513368+SCL3@users.noreply.github.com>
2023-12-12 13:20:38 +0100

    Add files via upload

--- AEF_deterministe.py
+++ AEF_deterministe.py
@@ -1,14 +1,19 @@
 from classes import Automate, State
 
+# Function to check if the automata is determinist : 
+# Condition 1 : Only 1 initial state
+# Condition 2 : 0 or 1 transition using a symbol of the alphabet for each state
 def isDeterminist(automate):
-    countitup=0
+    initState_count = 0
     for state in automate.states :
-        for symbs, transi in state.transitions.items() :  #le symbs, transi correspond aux clés/valeurs du dictionnaire transitions, items()permet de les parcourir
-            if len(transi)>1 :                            
-                return False
-        if state.isInitial == True :
-            countitup+=1
-    if countitup != 1 :
+        for transi in state.transitions.values() :  #le symbs, transi correspond aux clés/valeurs du dictionnaire transitions, items()permet de les parcourir
+            print(transi)
+            if len(transi) != 0 and len(transi) != 1 :  # Condition 2 : Check for each state if there's only 0 or 1 transition                        
+                return False    
+        if state.isInitial == True :  # Check the first condition
+            initState_count += 1
+    # Condition 1 : Check if there is only 1 initial State
+    if initState_count != 1 :
         return False
     return True
     
@@ -34,3 +39,16 @@ def determining(automate):
     for state in automate.states :
         for symbs, transi in state.transitions.items() :  #le symbs, transi correspond aux clés/valeurs du dictionnaire transitions, items()permet de les parcourir
             print('aah')
+
+alphabet = ['a', 'b']
+
+q0 = State('q0', True, False, {'a':['q3'], 'b':['q1']})
+q1 = State('q1', False, False, {'a':['q1'], 'b':['q2']})
+q2 = State('q2', False, True, {})
+q3 = State('q3', False, True, {'a':['q3']})
+
+a = Automate(alphabet, [q0, q1, q2, q3], "test")
+if isDeterminist(a):
+    print("oui")
+else:
+    print("non")
