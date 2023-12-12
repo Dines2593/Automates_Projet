573462b53663d610755c63ae6fca6b5e02c8628a
WooWoo <148549905+woowooStacked@users.noreply.github.com>
2023-12-12 09:12:17 +0100

    Update AEF_deterministe.py

--- AEF_deterministe.py
+++ AEF_deterministe.py
@@ -1,21 +1,36 @@
 from classes import Automate, State
 
-def determine(automate):
-    is_determinist = True
+def isDeterminist(automate):
+    countitup=0
     for state in automate.states :
         for symbs, transi in state.transitions.items() :  #le symbs, transi correspond aux clés/valeurs du dictionnaire transitions, items()permet de les parcourir
             if len(transi)>1 :                            
-                is_determinist = False
-                break
+                return False
+        if state.isInitial == True :
+            countitup+=1
+    if countitup != 1 :
+        return False
+    return True
     
     if is_determinist :
         print("L'automate est déjà déterministe")
         
     else :
+        yeepee=0
+        while yeepee == 0:
+            chogoice=input(print("Lautomate n'est pas déterministe. Voulez vous le déterminiser?\n1:oui 2:non"))
+            match chogoice:
+                case 1:automate=determining(automate);yeepee=1
+                case 2:yeepee=1
+                case _:print("Le choix n'existe pas, veuillez rééssayer.\n")
 
-        print("pas deter")
-
-
-    
-
-    
+def determining(automate):
+    statesList=[]
+    transList=[]
+    for stagate in automate.states :
+        if stagate.isInitial == True :
+            statesList.append(stagate)
+            break
+    for state in automate.states :
+        for symbs, transi in state.transitions.items() :  #le symbs, transi correspond aux clés/valeurs du dictionnaire transitions, items()permet de les parcourir
+            print('aah')
